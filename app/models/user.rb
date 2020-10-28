37d99c27dddf7a539a268edbb510c1502d594c30
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-10-28 17:45:25 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,19 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
-  validates :lastname_kana, presence: true
-  validates :firstname_kana, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :lastname, presence: true
+    validates :firstname, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
+    validates :lastname_kana, presence: true
+    validates :firstname_kana, presence: true
+  end
+
   validates :birthday, presence: true
 end
