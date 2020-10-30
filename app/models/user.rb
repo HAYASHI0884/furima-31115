468305d3aa3796a01f3c76236bcf9717526911fa
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-10-30 11:01:54 +0900

    with_optionの使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-    validates :lastname, presence: true
-    validates :firstname, presence: true
+    validates :lastname
+    validates :firstname
   end
 
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
-    validates :lastname_kana, presence: true
-    validates :firstname_kana, presence: true
+    validates :lastname_kana
+    validates :firstname_kana
   end
-
-  validates :birthday, presence: true
 end
