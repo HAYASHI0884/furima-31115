c1676621ed07e9e926143960e398f19c736e2af1
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-10-30 12:40:34 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,10 @@ class ItemsController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
