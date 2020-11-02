6d630383d34d0c75fd39b6a3a0dcf842bc3a1394
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-11-02 13:21:18 +0900

    Rubocopの実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name               {"item"}
-    introduction       {"introduction"}
-    price              {"300"}
-    category_id        {"2"}
-    item_condition_id  {"2"}
-    postage_payer_id   {"2"}
-    shipping_area_id   {"2"}
-    preparation_day_id {"2"}
+    name               { 'item' }
+    introduction       { 'introduction' }
+    price              { '300' }
+    category_id        { '2' }
+    item_condition_id  { '2' }
+    postage_payer_id   { '2' }
+    shipping_area_id   { '2' }
+    preparation_day_id { '2' }
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
