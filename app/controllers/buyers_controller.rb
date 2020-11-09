d5e5c88ece6dd3918f4119f692bf0474f4d21142
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-11-09 23:37:55 +0900

    パラメーターの設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,4 +2,34 @@ class BuyersController < ApplicationController
   def index
     @buyer_address = BuyerAddress.new
   end
+
+  def new
+
+  end
+
+  def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+     if @buyer_address.valid?
+       #pay_item
+       @buyer_address.save
+       redirect_to items_path
+     else
+       render action: :index
+     end
+  end
+
+  private
+
+  def buyer_params
+   params.require(:buyer_address).permit(:postal_code, :prefectures_id, :municipality, :address_number, :building_name, :phone_number, :item_id, :buyer_id, :number, :exp_month, :exp_year, :cvc).merge(user_id: current_user.id, authenticity_token: params[:authenticity_token])
+  end
+
+  #def pay_item
+    #Payjp.api_key = "sk_test_ffb5600e88b15723bec2e94f"
+      #Payjp::Charge.create(
+        #amount: order_params[:price]
+        #card: order_params[:token],
+        #currency: 'jpy'
+      #)
+  #end
 end
