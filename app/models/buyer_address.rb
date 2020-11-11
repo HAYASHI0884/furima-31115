f7011c05e20972f6fa3c6a72e00ec5b60934d509
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-11-11 17:14:02 +0900

    バリデーションの修正

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -4,12 +4,12 @@ class BuyerAddress
 
   with_options presence: true do
     validates :address_number
-    validates :prefectures_id
     validates :municipality
     validates :user_id
     validates :token
   end
 
+  validates :prefectures_id, presence: true, numericality: { other_than: 1 }
   validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :phone_number, presence: true, format: { with: /\A\d{11}\z/ }
 
